80a42987fbfe5ff67a1bae38fd6b358fb87c6d17
Shawn <shawnshsh6@gmail.com>
2020-10-11 00:03:10 +0900

    edit .gitignore

---
+++ build/precache-manifest.3afe93d55c11117874ad730dfbec7ceb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd2561c9e9a98052c4af28a70bc97974",
+    "url": "/index.html"
+  },
+  {
+    "revision": "555f7b69598ceef1856d",
+    "url": "/static/css/main.31aaa3d7.chunk.css"
+  },
+  {
+    "revision": "d3345e0e271ce48fd97f",
+    "url": "/static/js/2.96eacc7a.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.96eacc7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "555f7b69598ceef1856d",
+    "url": "/static/js/main.c93969e4.chunk.js"
+  },
+  {
+    "revision": "c8760abc549710398f3e",
+    "url": "/static/js/runtime-main.751d650b.js"
+  }
+]);
