19e257973f540e1fa3b025a2c8a26df00c5e4cfa
Shawn <shawnshsh6@gmail.com>
2020-10-18 10:51:11 +0900

    testing

--- src/pages/main/MainHome.js
+++ src/pages/main/MainHome.js
@@ -5,6 +5,8 @@ import "./MainHome.css";
 import Loading from "../../components/Loading";
 import LoginToUse from "../../components/LoginToUse";
 import VerifyEmail from "../../components/VerifyEmail";
+import FavoriteFilledIcon from "../../assets/icons/FavoriteFilledIcon";
+import FavoriteOutlinedIcon from "../../assets/icons/FavoriteOutlinedIcon";
 
 function MainHome() {
   const [init, setInit] = useState(false);
@@ -42,7 +44,7 @@ function MainHome() {
                   <img
                     className="content_body_image"
                     alt=""
-                    src="https://picsum.photos/1000/100"
+                    src="https://picsum.photos/5000/2000"
                   ></img>
                   <div className="content_body_text">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -50,27 +52,19 @@ function MainHome() {
                     ligula. Pellentesque sodales non risus vel tristique. Sed
                     odio est, dictum quis volutpat et, luctus nec lacus.
                     Curabitur pharetra, ex eleifend tempor pretium, enim lacus
-                    iaculis nulla, non semper lectus erat ut libero. Ut rhoncus
-                    cursus justo vel tincidunt. Aenean scelerisque sagittis
-                    ultricies. In blandit lacus ut purus efficitur, ut tincidunt
-                    lorem convallis. Mauris quam odio, elementum non felis in,
-                    cursus vestibulum lorem. Pellentesque tempus felis nisl,
-                    quis auctor dolor convallis at. Duis varius nunc diam, quis
-                    blandit justo eleifend id. Integer in nisi vestibulum,
-                    interdum turpis ac, vehicula felis. Nullam scelerisque
-                    cursus odio, quis vehicula sapien semper eu. Vestibulum
-                    finibus diam mi, eu mollis metus rutrum consequat. Mauris
-                    blandit felis arcu, a euismod lacus porta eget. Maecenas sem
-                    arcu, luctus vitae mi at, tincidunt facilisis risus.
-                    Curabitur condimentum leo nisi, in euismod dui finibus quis.
+                    iaculis nulla, non semper lectus erat ut libero.
                   </div>
                 </div>
                 <div className="content_end">
                   <div className="content_end_column">
-                    <div className="content_end_column_button"></div>
+                    <div className="content_end_column_button">
+                      <FavoriteFilledIcon />
+                    </div>
                   </div>
                   <div className="content_end_column">
-                    <div className="content_end_column_button"></div>
+                    <div className="content_end_column_button">
+                      <FavoriteOutlinedIcon />
+                    </div>
                   </div>
                 </div>
               </div>
